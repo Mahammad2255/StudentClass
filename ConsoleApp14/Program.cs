a1f049dcabec322562a3ff0d31f260bbd072790a
Mahammad2255 <[mahammadei@code.edu.az]>
2021-12-11 05:26:00 -0800

    Add project files.

---
+++ ConsoleApp14/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student("Mehemmed", 4, "Muhendis", "BMU", "ismayilov@mail.ru", 57787);
+            Console.WriteLine(student.FullInfo());
+            
+            
+            
+
+
+        }
+    }
+}
---
+++ ConsoleApp14/Student.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    class Student
+    {
+        public string _fullName;
+        public int _course;
+        public string _subject;
+        public string _university;
+        public string _email;
+        public int _phoneNumber;
+        public Student(string fullname, int course, string subject, string university, string email, int phoneNumber)
+        {
+            _fullName = fullname;
+            _course = course;
+            _subject = subject;
+            _university = university;
+            _email = email;
+            _phoneNumber = phoneNumber;
+
+
+        }
+       
+
+        public string FullInfo()
+        {
+            return $"{_fullName} {_course} {_subject} {_university} {_email} {_phoneNumber}";
+
+        }
+    }
+}
